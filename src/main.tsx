4a9d8942fdda8db347249a205f78c44161a65931
HyeJin <yhj970102@gmail.com>
2026-07-30 17:36:07 +0900

    📦️ Chore : 패키지 설치 (#1)
    * 📦️ Chore : three.js 설치
    
    * 📦️ Chore : TailwindCSS 설치
    
    * 📦️ Chore : jinni-labs/ui 설치
    
    * 📦️ Chore :  gsap 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./main.css";
+import JinniProvider, {
+  createDesignSystem,
+} from "@jinni-labs/ui/JinniProvider";
+
+const designSystem = createDesignSystem();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <JinniProvider designSystem={designSystem}>
+      <App />
+    </JinniProvider>
   </StrictMode>,
 );
