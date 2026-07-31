1617ae9bb48eb9181292925ced190a4aa0b1517d
hyejin <yhj970102@gmail.com>
2026-07-31 18:42:50 +0900

    ✨ Feat : webGL 지원 여부 확인

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 import PlaygroundDrawer from "@/components/PlaygroundDrawer";
 import OpenDrawerButton from "@/components/OpenDrawerButton";
 import Canvas from "./components/Canvas";
+import WebGL from "three/addons/capabilities/WebGL.js";
 
 function App() {
   const [open, setOpen] = useState<boolean>(true);
 
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
+
+  if (!WebGL.isWebGL2Available()) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center text-lg font-bold">
+        Your browser does not support WebGL2!
+      </div>
+    );
+  }
 
   return (
     <main className="relative max-w-screen h-screen">
