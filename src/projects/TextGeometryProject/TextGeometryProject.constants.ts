7428ba7ffd9baaaecf80583d35893cc7b5a4ec95
hyejin <yhj970102@gmail.com>
2026-08-13 18:52:22 +0900

    🐛 Fix :  폰트 파일을 public 폴더로 이동

--- src/projects/TextGeometryProject/TextGeometryProject.constants.ts
+++ src/projects/TextGeometryProject/TextGeometryProject.constants.ts
@@ -1,9 +1,9 @@
 export const FONT_URLS = {
-  sans: "src/assets/fonts/droid_sans_regular.typeface.json",
-  serif: "src/assets/fonts/droid_serif_regular.typeface.json",
-  gentilis: "src/assets/fonts/gentilis_regular.typeface.json",
-  helvetiker: "src/assets/fonts/helvetiker_regular.typeface.json",
-  optimer: "src/assets/fonts/optimer_regular.typeface.json",
+  sans: "/fonts/droid_sans_regular.typeface.json",
+  serif: "/fonts/droid_serif_regular.typeface.json",
+  gentilis: "/fonts/gentilis_regular.typeface.json",
+  helvetiker: "/fonts/helvetiker_regular.typeface.json",
+  optimer: "/fonts/optimer_regular.typeface.json",
 };
 
 export const DIRECTION = ["ltr", "rtl", "tb"] as const;
