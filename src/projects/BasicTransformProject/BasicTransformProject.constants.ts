ac958334a7154412d2f27dd089056ab937818798
HyeJin <yhj970102@gmail.com>
2026-09-15 20:50:41 +0900

    ✨ Feat : 'Basics of transform' project 구현 (#22)

---
+++ src/projects/BasicTransformProject/BasicTransformProject.constants.ts
@@ -0,0 +1,10 @@
+export const IMAGES = [
+  "/textures/cube/red.jpeg",
+  "/textures/cube/yellow.jpeg",
+  "/textures/cube/green.jpeg",
+  "/textures/cube/blue.jpeg",
+  "/textures/cube/purple.jpeg",
+  "/textures/cube/brown.jpeg",
+];
+
+export const ROTATION_AXES = ["Local Axes", "Parent Axes"] as const;
